6409e81e6f5346b4c55e062de28c93913a5f069d
HiHiYoyo606 <144301049+HiHiYoyo606@users.noreply.github.com>
2025-03-17 23:08:07 +0800

    Update discordBot_HiHi.py

--- discordBot_HiHi.py
+++ discordBot_HiHi.py
@@ -29,7 +29,8 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 chat = model.start_chat(history=[])
 
 TARGET_CHANNEL_IDS = [
-    1351068939173498943,  
+    1351068939173498943,
+    1351206275538485424,
 ]
 
 class UserType(Enum):
