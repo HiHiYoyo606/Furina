b26a27ec8bd17d98de569dbb4bb17ecc37bb95bd
HiHiYoyo606_PC <kyuchiang@gmail.com>
2025-07-10 17:21:28 +0800

    v1.3.6a new website

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ async function loadSongList(tag) {
   try {
     const response = await fetch(`${tag}songs.txt`)
     const text = await response.text();
+    const lastModified = response.headers.get("Last-Modified");
+
+    if (lastModified) {
+      const date = new Date(lastModified);
+      const formatted = date.toLocaleString('sv-SE').replace(' ', ' | Last update: ');
+      document.getElementById('last-updated').textContent = `最後更新時間 | ${formatted}`;
+    } else {
+      document.getElementById('last-updated').textContent = "最後更新時間 | 無法取得";
+    }
     const songs = text.split('\n').map(line => line.trim()).filter(Boolean);
 
     // 建立表格行，每行 4 首歌
